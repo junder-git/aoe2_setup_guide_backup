a08e51bdc6e3e7b328b1f958c98cf9badd477a58
jabl3s_JTOP <j@jabl3s.uk>
2023-09-16 08:22:41 +0100

    reduce

--- main2.py
+++ main2.py
@@ -8,6 +8,4 @@ print("...OR...")
 print(f"Effectively buy {tribute:.0f} tribute of food for: {tribute/100*unitrate:.0f} gold instead... so why/when to buy...\n"
       f"Food diff equation::: \nfor {polefood-tribute:.0f} food lost in {polefood} tribute "
       f"its actually worth {(polefood-tribute)/100*unitrate:.0f} "
-      f"its weight in gold to the saracen..."
-      f"gold gen is much faster then food gen and by feudal age saracen will be on gold gen,"
-      f"with food gen so low as it is the need is there to buy that additional 1500 food")
+      f"its weight in gold to the saracen...")
