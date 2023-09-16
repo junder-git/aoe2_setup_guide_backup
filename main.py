b0bdb64feb8498f5d2fa9307a767fbfa5bb35e5b
jabl3s_JTOP <j@jabl3s.uk>
2023-09-16 03:27:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ interval = 3
 total = 0
 
 for num in range(start, end + 1, interval):
-    total += num
+    total += num*1.05
 
-print("The sum of integers from 70 to 100 in intervals of 3 is:", total)
+print("The sum of integers from 70 to 100 in intervals of 3 plus 5% per saracen interval is:", total)
